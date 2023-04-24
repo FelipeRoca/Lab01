507ed4bfbac4c8174fb4767518e3e7d22f08ad0d
Usuario <Usuario@DESKTOP-F4CK7IQ>
2023-04-24 15:00:55 -0300

    Agregar archivos de proyecto.

---
+++ Iteracion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iteracion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cantIteraciones = 5;
+            String[] arregloDeStrings = new String[cantIteraciones];
+
+            for (int i=0; i<cantIteraciones; i++)
+            {
+                Console.Write("Ingrese palabra " + (i+1) + ": ");
+                arregloDeStrings[i] = Console.ReadLine();
+            }
+
+            for (int i=cantIteraciones; i > 0; i--)
+            {
+                Console.Write("- ");
+                Console.WriteLine(arregloDeStrings[i-1]);
+            }
+
+        }
+    }
+}
---
+++ Lab01/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+/*EJERCICIO 1*/
+
+
+            Console.WriteLine("Ingrese un texto: ");
+           String inputTexto = Console.ReadLine();
+
+           if (inputTexto ==  "") { 
+                Console.WriteLine("Texto vacio");
+                Console.ReadKey();
+
+            }
+           else
+            {
+                Console.Write("El texto es: ");
+                Console.WriteLine(inputTexto);
+
+                Console.WriteLine("1. Mostrar en Mayusculas");
+                Console.WriteLine("2. Mostrar en Minusculas");
+                Console.WriteLine("3. Mostrar cantidad de caracteres");
+
+                ConsoleKeyInfo opcion = Console.ReadKey();
+                while (opcion.Key != ConsoleKey.D1 && opcion.Key != ConsoleKey.D2 && opcion.Key != ConsoleKey.D3) { 
+                opcion = Console.ReadKey();
+                }
+
+                Console.WriteLine();
+
+               /* if (opcion.Key == ConsoleKey.D1)
+                {
+                    Console.WriteLine(inputTexto.ToUpper());
+                }
+                else if (opcion.Key == ConsoleKey.D2)
+                {
+                    Console.WriteLine(inputTexto.ToLower());
+                }
+                else
+                {
+                    Console.WriteLine(inputTexto.Length);
+                } */
+
+ /*EJERCICIO 2*/
+                
+                
+                switch (opcion.Key)
+                {
+                    case ConsoleKey.D1:
+
+                        Console.WriteLine(inputTexto.ToUpper());
+                        break;
+                    case ConsoleKey.D2:
+                        Console.WriteLine(inputTexto.ToLower());
+                        break;
+                    default:
+                        Console.WriteLine(inputTexto.Length);
+                        break;
+                }
+
+            }
+        }
+    }
+}
